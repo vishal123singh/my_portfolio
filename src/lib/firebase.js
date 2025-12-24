d3a69d2812cd0c9c3f3d340f69cefa44c7a9067b
Vishal Singh <bs08081997@gmail.com>
2025-12-25 03:11:50 +0530

    bug fixes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,7 +8,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log("Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
